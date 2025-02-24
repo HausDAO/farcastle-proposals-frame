2850365cf23dd78510099ee540530533188446c2
skuhlmann <samkuhlmann@gmail.com>
2025-02-24 13:56:42 -0700

    test some meta changes

--- src/app/dao/[chainid]/[daoid]/page.tsx
+++ src/app/dao/[chainid]/[daoid]/page.tsx
@@ -14,26 +14,70 @@ type Props = {
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { chainid, daoid } = await params;
 
+  // const frame = {
+  //   version: "next",
+  //   imageUrl: `https://${process.env.NEXT_PUBLIC_URL}/image.png`,
+  //   button: {
+  //     title: "Make Proposal",
+  //     action: {
+  //       type: "launch_frame",
+  //       name: "Proposals",
+  //       url: `https://${process.env.NEXT_PUBLIC_URL}`,
+  //       iconImageUrl: `https://${process.env.NEXT_PUBLIC_URL}/icon.png`,
+  //       splashImageUrl: `https://${process.env.NEXT_PUBLIC_URL}/splash.png`,
+  //       splashBackgroundColor: "#341A34",
+  //     },
+  //   },
+  // };
+
+  // export async function generateMetadata(): Promise<Metadata> {
+  //   return {
+  //     metadataBase: new URL(`https://${process.env.NEXT_PUBLIC_URL}`),
+  //     title: "Proposals",
+  //     openGraph: {
+  //       title: "Farcastle Proposals",
+  //       description: "the actions of organizations",
+  //       images: `https://${process.env.NEXT_PUBLIC_URL}/image.png`,
+  //     },
+  //     other: {
+  //       "fc:frame": JSON.stringify(frame),
+  //     },
+  //   };
+  // }
+
   const frame = {
     version: "next",
-    imageUrl: `${appUrl}/opengraph-image`,
+    imageUrl: `https://${process.env.NEXT_PUBLIC_URL}/image.png`,
     button: {
-      title: "Launch",
+      title: "Make DAO Proposal",
       action: {
         type: "launch_frame",
-        name: "#3 Farcastle DAO Proposals",
+        name: "DAO Proposals",
         url: `${appUrl}/dao/${chainid}/${daoid}`,
         splashImageUrl: `${appUrl}/splash.png`,
-        splashBackgroundColor: "#17151F",
+        splashBackgroundColor: "#341A34",
       },
     },
   };
 
+  // return {
+  //   title: "#1 Farcastle DAO Proposals",
+  //   openGraph: {
+  //     title: "#2 Farcastle DAO Proposals",
+  //     description: "Farcastle DAO Proposals",
+  //   },
+  //   other: {
+  //     "fc:frame": JSON.stringify(frame),
+  //   },
+  // };
+
   return {
-    title: "#1 Farcastle DAO Proposals",
+    metadataBase: new URL(`https://${process.env.NEXT_PUBLIC_URL}`),
+    title: "DAO Proposals",
     openGraph: {
-      title: "#2 Farcastle DAO Proposals",
-      description: "Farcastle DAO Proposals",
+      title: "Farcastle DAO Proposals",
+      description: "the actions of organizations",
+      images: `https://${process.env.NEXT_PUBLIC_URL}/image.png`,
     },
     other: {
       "fc:frame": JSON.stringify(frame),
