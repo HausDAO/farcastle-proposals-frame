7c00698624965985fc9910d0a6fe9ff68f1b6a34
skuhlmann <samkuhlmann@gmail.com>
2025-02-24 13:11:53 -0700

    try again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,15 @@ import { Providers } from "@/providers/Providers";
 
 const frame = {
   version: "next",
-  imageUrl: `https://proposals.farcastle.net/image.png`,
+  imageUrl: `https://${process.env.NEXT_PUBLIC_URL}/image.png`,
   button: {
     title: "Make Proposal",
     action: {
       type: "launch_frame",
       name: "Proposals",
-      url: "https://proposals.farcastle.net",
-      iconImageUrl: `https://proposals.farcastle.net/icon.png`,
-      splashImageUrl: `https://proposals.farcastle.net/splash.png`,
+      url: "https://${process.env.NEXT_PUBLIC_URL}",
+      iconImageUrl: `https://${process.env.NEXT_PUBLIC_URL}/icon.png`,
+      splashImageUrl: `https://${process.env.NEXT_PUBLIC_URL}/splash.png`,
       splashBackgroundColor: "#341A34",
     },
   },
@@ -23,12 +23,12 @@ const frame = {
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    metadataBase: new URL("https://proposals.farcastle.net"),
+    metadataBase: new URL("https://${process.env.NEXT_PUBLIC_URL}"),
     title: "Proposals",
     openGraph: {
       title: "Farcastle Proposals",
       description: "the actions of organizations",
-      images: `https://proposals.farcastle.net/image.png`,
+      images: `https://${process.env.NEXT_PUBLIC_URL}/image.png`,
     },
     other: {
       "fc:frame": JSON.stringify(frame),
