ce6f81ba4df933627da975e2852ea529bbdb5d64
skuhlmann <samkuhlmann@gmail.com>
2025-02-24 13:41:43 -0700

    removes some logs

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -7,7 +7,6 @@ export const getGraphUrl = ({
   graphKey: string;
   subgraphKey: string;
 }): string => {
-  console.log("getGraphUrl chainid", chainid);
   const subgraphHash = SUBGRAPH_IDS[subgraphKey][chainid];
   if (!subgraphHash) {
     console.log("invalid chainid or subgraphkey");
