4798b8b117c3d0e04feabc55ee9e9e51dfa7d9b8
e2t <accounts@earth2travis.com>
2025-06-11 14:22:07 -0500

    style: tweak proposal details design

--- src/app/dao/[chainid]/[daoid]/proposal/[proposalid]/proposal-detail.tsx
+++ src/app/dao/[chainid]/[daoid]/proposal/[proposalid]/proposal-detail.tsx
@@ -3,6 +3,7 @@
 import sdk from "@farcaster/frame-sdk";
 import { useCallback, useEffect, useState } from "react";
 import { useAccount, useChainId, useSwitchChain } from "wagmi";
+import { format } from "date-fns";
 
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
@@ -115,64 +116,64 @@ export default function ProposalDetail() {
   return (
     <div className="w-full h-full space-y-4 pb-4 px-4">
       <Card className="flex flex-col items-center px-4 pt-4 pb-8 rounded-none">
-        <div className="text-muted font-display text-lg uppercase w-full text-left">
+        <div className="text-muted text-xs mb-1 uppercase w-full text-left">
           {`${getProposalTypeLabel(proposal.proposalType)} | ${proposalid}`}
         </div>
 
-        <div className="text-primary font-display text-4xl uppercase w-full text-left">
+        <div className="text-primary font-display text-2xl mb-1 uppercase w-full text-left">
           {proposal.title}
         </div>
         {proposal.description && (
-          <div className="text-muted font-mulish text-xl mb-4 w-full text-left">
+          <div className="leading-relaxed font-mulish mb-4 w-full text-left">
             {truncateString(proposal.description, 400)}
           </div>
         )}
 
-        <div className="flex flex-row justify-between mb-4 w-full">
+        <div className="flex flex-row justify-between mb-2 w-full">
           <div>
-            <div className="text-primary font-display text-xl  w-full text-left">
+            <div className="text-muted text-xs mb-1 uppercase w-full text-left">
               Submitted by
             </div>
-            <div className="text-muted font-mulish text-base w-full text-left">
+            <div className="font-mulish text-base w-full text-left">
               {truncateAddress(proposal.proposedBy)}
             </div>
           </div>
           <div>
-            <div className="text-primary font-display text-xl  w-full text-left">
+            <div className="text-muted text-xs mb-1 uppercase w-full text-right">
               Voting ends
             </div>
-            <div className="text-muted font-mulish text-base w-full text-left">
-              {truncateAddress(proposal.proposedBy)}
+            <div className="font-mulish text-base w-full text-right">
+            {format(new Date(Number(proposal.votingEnds) * 1000), "MMMM dd, HH:mm z")}
             </div>
           </div>
         </div>
 
-        <div className="mb-4">
-          <div className="text-desructive font-mulish text-xl w-full text-left">
+        <div className="mb-1">
+          <div className="text-muted text-xs uppercase w-full">
             {proposalStateText(proposal, status)}
           </div>
 
           {status === PROPOSAL_STATUS.needsProcessing && (
-            <div className="text-red-500 font-mulish text-sm w-full text-left">
+            <div className="text-destructive font-mulish text-sm w-full text-left">
               * Proposals must be executed in order
             </div>
           )}
         </div>
 
-        <div className="flex flex-row justify-between mb-4 w-full">
+        <div className="flex flex-row justify-between mb-1 w-full">
           <div>
-            <div className="text-primary font-display text-4xl  w-full text-left">
+            <div className="text-primary font-display text-2xl uppercase w-full text-left -mb-2">
               YES
             </div>
-            <div className="text-green-500 font-mulish text-4xl w-full text-left">
+            <div className="text-success font-display text-4xl w-full text-left">
               {propVotes.yes}
             </div>
           </div>
           <div>
-            <div className="text-primary font-display text-4xl  w-full text-left">
+            <div className="text-primary font-display text-2xl uppercase w-full text-right -mb-2">
               NO
             </div>
-            <div className="text-red-500 font-mulish text-4xl w-full text-left">
+            <div className="text-destructive font-display text-4xl w-full text-right">
               {propVotes.no}
             </div>
           </div>
@@ -194,14 +195,14 @@ export default function ProposalDetail() {
 
         <Button
           onClick={openProposalCastUrl}
-          variant="outline"
+          variant="secondary"
           className="w-full mt-4"
         >
           Cast Proposal
         </Button>
 
-        <Button onClick={openUrl} variant="outline" className="w-full mt-4">
-          Audit Proposal Details
+        <Button onClick={openUrl} variant="tertiary" className="w-full mt-4">
+          Audit Proposal
         </Button>
 
         {isConnected && !validChain && (
